3a83d538c5eb0edb481cc7d7030c4364655ccdfa
Giuseppe Mattia Greco <giumatt99@gmail.com>
2025-01-24 18:43:39 +0100

    Updated run script

--- run.sh
+++ run.sh
@@ -3,12 +3,18 @@
 echo "Choose the architecture to launch:"
 echo "1) 32-bit (runpst32)"
 echo "2) 64-bit (runpst64)"
+echo "3) 32-bit (runpst32_omp)"
+echo "4) 64-bit (runpst64_omp)"
 read -p "Type the number of your choice (1 or 2): " choice
 
 if [[ "$choice" == "1" ]]; then
     command="./runpst32"
 elif [[ "$choice" == "2" ]]; then
     command="./runpst64"
+elif [[ "$choice" == "3" ]]; then
+    command="./compile32omp"
+elif [[ "$choice" == "4" ]]; then
+    command="./compile64_omp"
 else
     echo "Invalid choice. Exiting."
     exit 1
@@ -22,6 +28,10 @@ ALPHA=1
 SD=3
 
 if [[ "$command" == "./runpst64" ]]; then
+    "$command" -seq "$SEQ" -to "$TO" -k "$K" -alpha "$ALPHA" -sd "$SD" -d
+elif [[ "$choice" == "./compile32omp" ]]; then
+    "$command" -seq "$SEQ" -to "$TO" -k "$K" -alpha "$ALPHA" -sd "$SD" -d
+elif [[ "$choice" == "./compile64_omp" ]]; then
     "$command" -seq "$SEQ" -to "$TO" -k "$K" -alpha "$ALPHA" -sd "$SD" -d
 else
     "$command" -seq "$SEQ" -to "$TO" -k "$K" -alpha "$ALPHA" -sd "$SD" -d
